845430fa1c8b671a4e4b8499daa5e14b7d943713
Julie Brosse <juliebrosse31@gmail.com>
2018-09-17 14:45:11 +0200

    bootstrp and flexbox installed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/chauffage'
   get 'pages/climatisation'
   get 'pages/contact'
-  get 'pages/about', to: 'pages#home', as: :about
+  get 'pages/about'
+  root to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
