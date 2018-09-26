68e23976c28b6afee1c51d89c15629b4cdfe1a67
Julie Brosse <juliebrosse31@gmail.com>
2018-09-26 16:08:33 +0200

    ok new style

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'pages/eau_chaude_sanitaires'
   get 'pages/chauffage'
   get 'pages/climatisation'
+  get 'pages/climatisation_test'
   get 'pages/contact'
-  get 'pages/about'
+  get 'pages/test'
+  get 'pages/chauffage_test'
+  get 'pages/about_test'
   root to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
