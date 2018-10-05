3747fe5500edf2bcec52fa4349d6a42b0d7f356a
Samia Dahmouni <dsamia@live.fr>
2018-10-05 16:56:15 +0200

    added controller and form

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   get 'pages/chauffage_test'
   root to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :contacts, only: %i[new create]
+
 end
